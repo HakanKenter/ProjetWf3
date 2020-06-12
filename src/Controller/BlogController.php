b81beaa9deba23ba27f1bbc2c0501ae51ed174b8
Prosper <amankwahprosper@gmail.com>
2020-06-12 12:41:45 +0200

    deco&reco+header_User/Visiteur

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -65,5 +65,5 @@ class BlogController extends AbstractController
         return $this->render('blog/create.html.twig', [
             'formAnnonce' => $form->createView()
         ]);
-    }
+    } 
 }
