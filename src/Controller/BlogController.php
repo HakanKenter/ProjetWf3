f06151a68af56b8eb1c36f028222c2035845f90e
malamineba <ma_la_ba@yahoo.com>
2020-06-14 17:48:10 +0200

    ajout des fonction modier et supprimer annonces

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -16,7 +16,7 @@ class BlogController extends AbstractController
 {
     
       /**
-     * @Route("/blog/new", name="blog_create")
+     * @Route("/new", name="blog_create")
      * @Route("/blog/{id}/edit", name="blogEdit")
      */
     public function formulaire(Annonce $annonce = null, Request $request, EntityManagerInterface $manager)
@@ -45,16 +45,25 @@ class BlogController extends AbstractController
             return $this->redirectToRoute('blog_show', ['id' => $annonce->getId()]);
         }
         // dump($annonce);
-        return $this->render('blog/depot_annonce.html.twig',[
+        return $this->render('blog/annonce.html.twig',[
             'formAnnonce' => $form->createView(),
             'editMode' => $annonce->getId()!== null
         ]);
+
+        
     }
 
-
-
+     /**
+     * @Route("/blog/{id}/delete", name="delete")
+     */
+    public function delete(Annonce $annonce)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($annonce);
+        $em->flush();
+        return $this->redirectToRoute('blog');
+    }
     
-
 
     /**
      * @Route("/blog/{id}", name="blog_show")
@@ -66,8 +75,9 @@ class BlogController extends AbstractController
          $annonce = $repo->find($id);
 
         // return $this->render('blog/show.html.twig');
-        return $this->render('blog/show.html.twig',['annonce'=> $annonce ]);
+        return $this->render('blog/show.html.twig',['annonce'=> $annonce]);
     }
+    
 
 
 
@@ -128,7 +138,7 @@ class BlogController extends AbstractController
     // }
 
     /**
-     * @Route("/blog/{id}", name="annonce_personnel")
+     * @Route("/blogg/{id}", name="annonce_personnel")
      */
     public function annoncePersonnel(User $user)
     {
--- src/Form/Annonce2Type.php
+++ src/Form/Annonce2Type.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Annonce;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class Annonce2Type extends AbstractType
 {
@@ -15,9 +18,11 @@ class Annonce2Type extends AbstractType
             ->add('title')
             ->add('Prix')
             ->add('Image')
-            ->add('CreatedAt')
-            ->add('user')
-        ;
+            ->add('user', TextType::class)
+            
+            ;
+        
+            
     }
 
     public function configureOptions(OptionsResolver $resolver)
